5c6c24bf9fb1a37ac41d33c0439ba3841004b6c2
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-10-20 17:27:03 +0600

    check is applier is a candidate

--- controllers/applyController.js
+++ controllers/applyController.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const User = require('../models/userSchema');
 const Job = require('../models/jobSchema');
+const Apply = require('../models/applySchema');
 
 const apply = async (req, res) => {
     try {
@@ -19,11 +20,20 @@ const apply = async (req, res) => {
             return res.status(500).send({ message: 'Application closed!', success: false });
         }
         
+        // verify candidate
+        const verifyCandidate = await User.findOne({ _id: req.body.candidate });
+        if (verifyCandidate.role !== 'candidate') return res.status(404).send({ message: 'Candidate not found!', success: false });
+
+
+        const apply = new Apply(req.body);
+        const result = await apply.save();
+        
 
         //push job id to user 
         // await User.findOneAndUpdate({ _id: req.body.createdBy }, {
         //     $push: { createdJobs: result._id }
         // }, { new: true })
+        // res.status(200).json(req.files)
 
         res.send({ verifyJob, message: 'Successfully created job', success: true });
     } catch (error) {
